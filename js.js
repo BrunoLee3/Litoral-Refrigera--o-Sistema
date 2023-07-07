9ee4b4594c3dbd1840631cd317f61a765e1342a9
BrunoLee3 <lee.bruno23@gmail.com>
2023-07-07 16:12:39 -0300

    init

---
+++ js.js
@@ -0,0 +1,69 @@
+function validaServico(idCod, idNome, idData, idOrc, idServ) {
+    let codigo = document.getElementById(idCod).value;
+    let nomeCliente = document.getElementById(idNome).value;
+    let date = document.getElementById(idData).value;
+    let orcamento = document.getElementById(idOrc).value;
+    let ser = document.getElementById(idServ).value
+    
+
+    if(codigo == "")
+        alert("Erro. Código não pode estar em branco.");
+    
+    else if(nomeCliente == "")
+        alert("Erro. Nome do Cliente não pode estar em branco.");
+    
+    else if(date == "")
+        alert("Erro. Data não pode estar em branco.");
+    
+    else if(orcamento == "")
+        alert("Erro. orcamento não pode estar em branco.");
+    
+    else if(ser == "")
+        alert("Erro. serviço não pode estar em branco.");
+    
+    else
+        salvaServico(codigo, nomeCliente, date, parseFloat(orcamento), ser);
+
+}
+
+function salvaServico(cod, cliente, date, orcamento, servico){
+    let novoServico = {codigo:cod, nome:cliente, data:date, orcamento: orcamento, Servico:servico};
+    
+     if(typeof(Storage) !== "undefined"){
+        let servicos = localStorage.getItem("servicos");
+        if(servicos == null) servicos = [];  
+        else
+            servicos = JSON.parse(servicos);
+            servicos.push(novoServico);
+            localStorage.setItem("servicos", JSON.stringify(servicos))
+            alert("Serviço cadastrado com sucesso!");
+            location.reload();
+    }
+    else alert("A versão do seu navegador é muito antiga. Por isso, não será possível executar essa aplicação");
+
+}
+
+
+function exibeServico(){
+    let servicos = localStorage.getItem("servicos")
+    if(servicos==null){
+       document.write("<h2>Não há nenhum serviço cadastrado</h2>");
+    }
+    else{
+        servicos = JSON.parse(servicos);
+        let contador = 1;
+        document.write("<h1>Lista de Atendimentos Registrados</h1>");
+        
+        servicos.forEach(servs => {
+            document.write("<ul>");
+            document.write("<li><strong>Código: </strong>"+ contador + "</li>");
+            document.write("<li><strong>Nome: </strong>" +servs.nome+ "</li>");
+            document.write("<li><strong>Data: </strong>" +servs.data+ "</li>");
+            document.write("<li><strong>Orçamento: </strong>" +servs.orcamento+ "</li>");
+            document.write("<li><strong>Serviço: </strong>" +servs.Servico+ "</li>");
+            document.write("<hr>")
+            document.write("</ul>");
+            contador++;
+        });
+    }
+}
